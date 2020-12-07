cfbf29dd39a7351b7a2d8ed9ac97aae4eda5accd
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2020-12-08 00:13:50 +0500

    Ground packages done

--- App.js
+++ App.js
@@ -2,20 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import 'react-native-gesture-handler';
+// import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <NavigationContainer>
+          <View>
+            <Text>Starting</Text>
+          </View>
+        </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

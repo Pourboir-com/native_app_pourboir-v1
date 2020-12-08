52bcc808d684d7a449065c0f5d4e705849e93337
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2020-12-09 02:12:37 +0500

    hotels list comp

--- src/dummyData/DummyData.js
+++ src/dummyData/DummyData.js
@@ -1,3 +1,6 @@
+const img1 = "https://www.samaa.tv/wp-content/uploads/2016/12/besthotelsites-1.jpg";
+const img2 = "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/izeD0hHEwWUc/v0/1000x-1.jpg";
+const img3 = "https://thebodrumbyphr.com/wp-content/uploads/2020/04/The-Bodrum-by-Paramount-Hotels-Resorts-11-1024x682.jpg";
 
 export const rateList = [
     {
@@ -12,7 +15,6 @@ export const rateList = [
       id: '3',
       heading: 'Service',
     },
-    
     {
         id: '4',
         heading: 'Professionalisme',
@@ -22,3 +24,46 @@ export const rateList = [
         heading: 'Voutre pourboir au serveur',
       },
   ];
+
+  export const placesList = [
+    {
+      id:1,
+      rate: 4/5,
+      name: 'Brasserie Le Soleil',
+      distance: '650m',
+      services: '13',
+      img: img1,
+    },
+    {
+      id:2,
+      rate: 4/5,
+      name: 'La Samaritaine',
+      distance: '650m',
+      services: '13',
+      img: img2,
+    },
+    {
+      id:3,
+      rate: 4/5,
+      name: 'Le Baletti',
+      distance: '650m',
+      services: '13',
+      img: img3,
+    },
+    {
+      id:4,
+      rate: 4/5,
+      name: 'Le petit Boileau',
+      distance: '650m',
+      services: '13',
+      img: img1,
+    },
+    {
+      id:5,
+      rate: 4/5,
+      name: 'Le Baletti',
+      distance: '650m',
+      services: '13',
+      img: img2,
+    },
+  ]
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View, StatusBar, Image, Dimensions, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ImageBackground, Image, Dimensions, TouchableOpacity, FlatList } from 'react-native';
 import { MaterialIcons, FontAwesome } from "@expo/vector-icons";
 import GlobalHeader from '../../components/GlobalHeader'
+import {placesList as LIST} from '../../dummyData/DummyData'
+import { Rating, AirbnbRating } from 'react-native-ratings';
 
 const Home = () => {
     return <View style={styles.container}>
@@ -13,9 +15,41 @@ const Home = () => {
         RightImg={true}
         />
         <StatusBar backgroundColor="orange" />
-        <Text style={{fontSize:22, marginTop:10,width:"90%", alignSelf:"center"}}>
+        <Text style={styles.txtHeading}>
             Autour de vous
         </Text>
+        <FlatList
+            data={LIST}
+            showsVerticalScrollIndicator={false}
+            numColumns={2}
+            keyExtractor={(item) => item._id}
+            renderItem={(itemData) => (
+                <View style={styles.viewItemConatier}>
+                    <ImageBackground style={styles.imgCard} source={{uri: itemData.item.img}}>
+                        <View style={{}}>
+                        <Rating
+                            // showRating
+                            // onFinishRating={this.ratingCompleted}
+                            style={{ paddingVertical: 0, alignSelf:"flex-start" }}
+                            // ratingBackgroundColor='blue'
+                            // ratingColor='green'
+                            imageSize={20}
+                            // startingValue={ratingCount/2}
+                            // readonly={true}
+                            ratingCount={5}
+                        />
+                        </View>
+                        <View style={{}}>
+                            <Text style={{color:"#fff", fontSize:17}}>{itemData.item.name}</Text>
+                            <View style={styles.view2Card}>
+                                <Text style={styles.txt2Card}>{itemData.item.distance}</Text>
+                                <Text style={styles.txt2Card}>{itemData.item.services} serveurs</Text>
+                            </View>
+                        </View>
+                    </ImageBackground>
+                </View>
+            )}
+            />
     </View>
 }
 export default Home;
@@ -23,8 +57,24 @@ export default Home;
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        // justifyContent:"center",
-        //  alignItems:'center',
         backgroundColor:"#fff"
     },
+    view2Card:{
+        flexDirection:"row", width:"100%", alignItems:"center", justifyContent:"space-between"
+    },
+    txt2Card:{
+        color:"#EDEFEE", fontSize:13
+    },
+    imgCard:{
+        flex:1, padding:12, justifyContent:'space-between'
+    },
+    viewItemConatier:{
+        width:Dimensions.get('window').width*0.44,
+        height:Dimensions.get('window').width*0.55,
+        margin:Dimensions.get('window').width*0.03, backgroundColor:"red",
+        borderRadius:12,overflow:"hidden"
+    },
+    txtHeading:{
+        fontSize:22, marginTop:10,width:"90%", alignSelf:"center"
+    }
 })

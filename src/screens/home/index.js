0157f5bc5e2886a27ff9e9351f92529dd4590487
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2020-12-16 03:19:39 +0500

    Searchbar updated

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -2,6 +2,8 @@ import { useNavigation } from '@react-navigation/native';
 import React, { useLayoutEffect, useRef, useState } from 'react';
 import { StyleSheet, Text, TextInput, View, ScrollView, Image, Dimensions, TouchableOpacity, StatusBar, FlatList } from 'react-native';
 import Animated, { Extrapolate } from 'react-native-reanimated';
+import { MaterialIcons, Feather, Entypo } from "@expo/vector-icons";
+
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import Svg, { ClipPath, Defs, G, Path } from 'react-native-svg';
 
@@ -16,12 +18,14 @@ import { placesList as LIST } from '../../dummyData/DummyData'
 import { Colors } from '../../constants/Theme';
 import HomeCard from '../../components/HomeCard';
 
+const NoListImg = require('../../assets/images/emptyRestaurantList.png')
 // import SkeletonContent from 'react-native-skeleton-content';
 
 const HEADER_HEIGHT = HEADER_BAR_HEIGHT * 3 + getStatusBarHeight();
 
 export default HomeScreen = (props) => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [searchIconPress, setsearchIconPress] = useState(false)
 
   const navigation = useNavigation();
 
@@ -62,35 +66,38 @@ export default HomeScreen = (props) => {
   useLayoutEffect(() => {
 
     const renderUserIcon = ({ }) => {
-
       // return <Ionicons name="ios-contact" size={30} onPress={(): void => propsUserIcon.navigation.navigate('SelectSignIn')} />;
       return (
-        <TouchableOpacity
-          // onPress={() => propsUserIcon.navigation.navigate('Setting')}
-          onPress={() => props.navigation.navigate('Setting')}
-        >
-          <SvgHeaderUserIcon height={HEADER_BAR_HEIGHT} />
-        </TouchableOpacity>
+        <View>
+          {searchIconPress ? null :
+            <TouchableOpacity
+              onPress={() => props.navigation.navigate('Setting')}
+            >
+              <SvgHeaderUserIcon height={HEADER_BAR_HEIGHT} />
+            </TouchableOpacity>
+          }
+        </View>
       );
     };
     const renderTitle = () => {
-
       return (
-        <Animated.View
-          style={{
-            alignItems: 'flex-start',
-            justifyContent: 'center',
-            marginLeft: titleHeaderMarginLeft,
-          }}
-        >
-          {/* <StatusBar hidden={true} /> */}
-          <Text style={{
-            // fontFamily: 'ProximaNova-Bold', 
-            fontSize: 20, color: COLORS[colorScheme].text.primary
-          }} ellipsizeMode="tail" numberOfLines={1}>
-            Bonjour '80' Vincent Delacourt
+        <View>
+          {searchIconPress ? null :
+            <Animated.View
+              style={{
+                alignItems: 'flex-start',
+                justifyContent: 'center',
+                marginLeft: titleHeaderMarginLeft,
+              }}
+            >
+              <Text style={{
+                fontSize: 20, color: COLORS[colorScheme].text.primary
+              }} ellipsizeMode="tail" numberOfLines={1}>
+                Bonjour '80' Vincent Delacourt
           </Text>
-        </Animated.View>
+            </Animated.View>
+          }
+        </View>
       );
     };
 
@@ -113,17 +120,33 @@ export default HomeScreen = (props) => {
       ItemsEven.push(LIST[i]);
     }
   }
-  const dummyArray = [1,2,3]
+  const dummyArray = [1, 2, 3]
 
   return (
     <>
       <Animated.ScrollView
         ref={scrollRef}
-        contentContainerStyle={{ paddingTop: HEADER_HEIGHT, minHeight: LAYOUT.window.height + HEADER_HEIGHT }}
+        contentContainerStyle={{ paddingTop: searchIconPress ? 0 : HEADER_HEIGHT, minHeight: searchIconPress ? 0 : LAYOUT.window.height + HEADER_HEIGHT }}
         scrollEventThrottle={1}
         showsVerticalScrollIndicator={false}
         onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollYAnimatedValue } } }])}
       >
+        {searchIconPress ?
+          <View style={styles.viewHeader2}>
+            <View style={styles.viewInputSearch}>
+              <TouchableOpacity
+                onPress={() => setsearchIconPress(!searchIconPress)}
+                style={{ paddingHorizontal: 8 }}
+              >
+                <Feather name="search" color={Colors.yellow} size={25} />
+              </TouchableOpacity>
+              <TextInput placeholder="Search" style={styles.inputSearch} />
+              <TouchableOpacity style={{ paddingHorizontal: 8 }}>
+                <Entypo name="circle-with-cross" color={Colors.yellow} size={25} />
+              </TouchableOpacity>
+            </View>
+          </View>
+          : null}
         {
           LIST.length === 0 ?
             <View style={styles.viewEmptyList}>
@@ -147,17 +170,17 @@ export default HomeScreen = (props) => {
             </Text>
               <View style={{ flexDirection: "row" }}>
                 <FlatList
-                  data={ loading ? dummyArray : ItemsOdd}
+                  data={loading ? dummyArray : ItemsOdd}
                   showsVerticalScrollIndicator={false}
                   keyExtractor={(item) => item._id}
                   renderItem={(itemData) => (
                     <HomeCard
                       navigation={navigation}
-                      img={ loading ? null : itemData.item.img}
-                      rating={ loading ? null : itemData.item.rate}
-                      name={ loading ? null : itemData.item.name}
-                      distance={ loading ? null : itemData.item.distance}
-                      services={ loading ? null : itemData.item.services}
+                      img={loading ? null : itemData.item.img}
+                      rating={loading ? null : itemData.item.rate}
+                      name={loading ? null : itemData.item.name}
+                      distance={loading ? null : itemData.item.distance}
+                      services={loading ? null : itemData.item.services}
                       loading={loading}
                     />
                   )}
@@ -170,11 +193,11 @@ export default HomeScreen = (props) => {
                   renderItem={(itemData) => (
                     <HomeCard
                       navigation={navigation}
-                      img={ loading ? null : itemData.item.img}
-                      rating={ loading ? null : itemData.item.rate}
-                      name={ loading ? null : itemData.item.name}
-                      distance={ loading ? null : itemData.item.distance}
-                      services={ loading ? null : itemData.item.services}
+                      img={loading ? null : itemData.item.img}
+                      rating={loading ? null : itemData.item.rate}
+                      name={loading ? null : itemData.item.name}
+                      distance={loading ? null : itemData.item.distance}
+                      services={loading ? null : itemData.item.services}
                       loading={loading}
                     />
                   )}
@@ -183,56 +206,55 @@ export default HomeScreen = (props) => {
             </ScrollView>
         }
       </Animated.ScrollView>
-      <Animated.View style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-      }}>
-        <Animated.View style={[{ position: 'relative', top: headerHeight, height: HEADER_HEIGHT }]}>
-          <Svg viewBox="0 0 375 190" preserveAspectRatio="none">
-            <Defs>
-              <ClipPath id="prefix__a">
-                <Path d="M0 0h375v170a20 20 0 01-20 20H20a20 20 0 01-20-20V0z" transform="translate(0 .981)" fill="#fcdf6f" />
-              </ClipPath>
-            </Defs>
-            <Path data-name="Mask" d="M0 0h375v170a20 20 0 01-20 20H20a20 20 0 01-20-20V0z" fill="#fcdf6f" />
-            <G data-name="BG" clipPath="url(#prefix__a)" transform="translate(0 -.981)">
-              <Path
-                data-name="Weird Shape"
-                d="M238.418-41.74c59.628-43.534 217.933 62.136 224.724 167.256s-148.41 240.355-200.258 210.42 13.883-143.786 11.81-189.565S178.788 1.791 238.417-41.74z"
-                fill="#ffe685"
-              />
-            </G>
-          </Svg>
-        </Animated.View>
+      {searchIconPress ? null :
+        <Animated.View style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+        }}>
+          <Animated.View style={[{ position: 'relative', top: headerHeight, height: HEADER_HEIGHT }]}>
+            <Svg viewBox="0 0 375 190" preserveAspectRatio="none">
+              <Defs>
+                <ClipPath id="prefix__a">
+                  <Path d="M0 0h375v170a20 20 0 01-20 20H20a20 20 0 01-20-20V0z" transform="translate(0 .981)" fill="#fcdf6f" />
+                </ClipPath>
+              </Defs>
+              <Path data-name="Mask" d="M0 0h375v170a20 20 0 01-20 20H20a20 20 0 01-20-20V0z" fill="#fcdf6f" />
+              <G data-name="BG" clipPath="url(#prefix__a)" transform="translate(0 -.981)">
+                <Path
+                  data-name="Weird Shape"
+                  d="M238.418-41.74c59.628-43.534 217.933 62.136 224.724 167.256s-148.41 240.355-200.258 210.42 13.883-143.786 11.81-189.565S178.788 1.791 238.417-41.74z"
+                  fill="#ffe685"
+                />
+              </G>
+            </Svg>
+          </Animated.View>
 
-        <Animated.View
-          style={{
-            position: 'absolute',
-            top: searchBarTop,
-            height: HEADER_BAR_HEIGHT,
-            width: searchBarWidth,
-            left: spacing(1.5),
-            borderRadius: HEADER_BAR_HEIGHT,
-            backgroundColor: searchBarColor
-          }}
-        >
-          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
-            <View style={{ paddingLeft: HEADER_BAR_HEIGHT / 4 }}>
-              <SvgHeaderSearchIcon />
+          <Animated.View
+            style={{
+              position: 'absolute',
+              top: searchBarTop,
+              height: HEADER_BAR_HEIGHT,
+              width: searchBarWidth,
+              left: spacing(1.5),
+              borderRadius: HEADER_BAR_HEIGHT,
+              backgroundColor: searchBarColor
+            }}
+          >
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
+              <TouchableOpacity 
+              onPress={()=> setsearchIconPress(!searchIconPress)}
+              style={{ paddingLeft: HEADER_BAR_HEIGHT / 4 }}>
+                <SvgHeaderSearchIcon />
+              </TouchableOpacity>
+              <TextInput
+                placeholder="Recherchez votre restaurant"
+                style={{ flex: 1, paddingHorizontal: 25 }} />
             </View>
-            <TextInput
-              placeholder="Recherchez votre restaurant"
-              style={{ flex: 1, paddingHorizontal: 25 }} />
-            {/* <Animated.Text style={[{
-               fontSize: 14,
-              //  fontFamily: 'ProximaNova-Regular',
-               color: COLORS[colorScheme].text.secondary,
-            }, { paddingLeft: spacing(1), opacity: searchBarOpacity }]}>Recherchez votre restaurant</Animated.Text> */}
-          </View>
+          </Animated.View>
         </Animated.View>
-      </Animated.View>
+      }
     </>
   );
 };
@@ -241,6 +263,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
+  },
+  inputSearch: {
+    height: 45, backgroundColor: "#fff",
+    paddingHorizontal: 5, paddingVertical: 0, flex: 1
+  },
+  viewInputSearch: {
+    flexDirection: "row", alignItems: "center", backgroundColor: "#fff", width: "90%",
+    marginTop: 15, alignSelf: "center", borderRadius: 7, overflow: "hidden"
+  },
+  viewHeader2: {
+    width: "100%", height: 80, backgroundColor: Colors.yellow, borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20
   },
   txt1NoRest: {
     fontSize: 16, color: Colors.fontDark, textAlign: "center", maxWidth: 190, marginTop: 20

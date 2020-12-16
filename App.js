2cf9c94dec68f716db0678064a1324059826ede3
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2020-12-16 05:31:24 +0500

    khs

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-// import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/navigation/AppNavigator';
 

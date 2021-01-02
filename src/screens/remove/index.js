e59125cbbadafbf1f3a5fdc060a917bfc7884641
YASIR AHMED <yasira640@gmail.com>
2021-01-02 05:48:23 +0500

    header border updated

--- src/screens/remove/index.js
+++ src/screens/remove/index.js
@@ -8,6 +8,7 @@ import GlobalHeader from '../../components/GlobalHeader';
 import HomeScreenContent from '../../components/HomeContent'
 
 import i18n from "../../li8n";
+import { View } from "react-native";
 export default Remove = (props) => {
     const [loading, setLoading] = useState(false);
     const [searchIconPress, setSearchIconPress] = useState(false);
@@ -16,18 +17,34 @@ export default Remove = (props) => {
 
     return (
         <>
-            <ImageBackground style={{ width: '100%', height: 100 }} source={require('../../assets/images/Group3.png')}>
+            <View style={{
+                width: '100%', height: 100,
+                backgroundColor: '#f9f9f9'
+            }}>
+                <View style={{
+                    width: '100%', height: 100,
+                    borderBottomLeftRadius: 25,
+                    borderBottomRightRadius: 25,
+                    overflow: 'hidden',
+                    backgroundColor: '#f9f9f9'
+                }}>
+                    <ImageBackground style={{
+                        width: '100%', height: 100,
+                    }} source={require('../../assets/images/Group3.png')}>
 
-                <GlobalHeader
-                    arrow={true}
-                    headingText={i18n.t('your_restaurant')}
-                    fontSize={17}
-                    color={Colors.fontDark}
-                    navigation={navigation}
-                    setting={true}
-                    backgroundColor={'transparent'}
-                />
-            </ImageBackground>
+                        <GlobalHeader
+                            arrow={true}
+                            headingText={i18n.t('your_restaurant')}
+                            fontSize={17}
+                            color={Colors.fontDark}
+                            navigation={navigation}
+                            setting={true}
+                            backgroundColor={'transparent'}
+                            borderRadius={true}
+                        />
+                    </ImageBackground>
+                </View>
+            </View>
 
             <HomeScreenContent
                 loading={loading}

16c0991d294628b7f40d715de694831bf6a9ca5b
Muhammad Salman Siddiqui <salmansidd991@gmail.com>
2021-01-18 08:22:13 +0500

    NoLocation request access

--- src/screens/NoLocationFound/index.js
+++ src/screens/NoLocationFound/index.js
@@ -1,59 +1,73 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Colors } from '../../constants/Theme';
-import { Entypo } from "@expo/vector-icons";
+import { Entypo } from '@expo/vector-icons';
 import * as Location from 'expo-location';
+import { useNavigation } from '@react-navigation/native';
 
 import i18n from '../../li8n';
 
 const NoLocation = () => {
-
   const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const navigation = useNavigation();
 
-  let text = 'Waiting..';
-  const excessLocation =async ()=>{
+  const excessLocation = async () => {
     let { status } = await Location.requestPermissionsAsync();
     if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
       return;
     }
-
     let location = await Location.getCurrentPositionAsync({});
     setLocation(location);
+    navigation.replace('Home', { crossIcon: false });
   };
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
 
-  return <View style={styles.container}>
-    <Entypo name="location-pin" size={220} color={Colors.yellow} style={{ marginBottom: -150, zIndex: 10 }} />
-    <View style={{ width: 150, height: 150, backgroundColor: '#fff', borderRadius: 100, zIndex: -10 }}></View>
-    <Text style={{ fontSize: 20, color: Colors.fontDark, marginTop: 20 }}>
-      {i18n.t('activate_your_geolocation')}
-    </Text>
-    <TouchableOpacity style={styles.btnStyle} onPress={excessLocation}>
-      <Text style={styles.txtColor}>
-        {i18n.t('activate_geoloc')}
+  return (
+    <View style={styles.container}>
+      <Entypo
+        name="location-pin"
+        size={220}
+        color={Colors.yellow}
+        style={{ marginBottom: -150, zIndex: 10 }}
+      />
+      <View
+        style={{
+          width: 150,
+          height: 150,
+          backgroundColor: '#fff',
+          borderRadius: 100,
+          zIndex: -10,
+        }}
+      ></View>
+      <Text style={{ fontSize: 20, color: Colors.fontDark, marginTop: 20 }}>
+        {i18n.t('activate_your_geolocation')}
       </Text>
-    </TouchableOpacity>
-  </View>;
+      <TouchableOpacity style={styles.btnStyle} onPress={excessLocation}>
+        <Text style={styles.txtColor}>{i18n.t('activate_geoloc')}</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 export default NoLocation;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, backgroundColor: "#EEEFEE",
-    justifyContent: "center", alignItems: "center",
+    flex: 1,
+    backgroundColor: '#EEEFEE',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   btnStyle: {
-    backgroundColor: Colors.yellow, width: "85%", position: "absolute",
-    bottom: 30, justifyContent: "center", alignItems: "center", paddingVertical: 15,
+    backgroundColor: Colors.yellow,
+    width: '85%',
+    position: 'absolute',
+    bottom: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 15,
     borderRadius: 5,
   },
   txtColor: {
-    color: Colors.fontLight, fontSize: 16,
+    color: Colors.fontLight,
+    fontSize: 16,
   },
 });

ade4abda477fa78646edd0c9d25c5cd924188765
Muhammad Salman Siddiqui <salmansidd991@gmail.com>
2021-01-21 09:17:59 +0500

    rate your service flows done

--- src/screens/rateYourService/index.js
+++ src/screens/rateYourService/index.js
@@ -66,6 +66,7 @@ const RateService = ({ navigation, route }) => {
 
   const handleModalClose = () => {
     setisVisible(false);
+    navigation.navigate('OpenCardReviews');
   };
 
   const handleModalOpen = () => {

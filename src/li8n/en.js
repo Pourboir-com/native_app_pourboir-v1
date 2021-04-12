b9176ea61b689ae2e90f6e8a1da4b8230b3eb6b6
Muhammad Salman Siddiqui <salmansidd991@gmail.com>
2021-04-12 19:39:29 +0500

    fixes

--- src/li8n/en.js
+++ src/li8n/en.js
@@ -12,7 +12,7 @@ export default {
   search_for_rest_and_add:
     'Search for your restaurant and add yourself by choosing: You are a waiter',
   no_restaurant_found: 'Sorry, no restaurant was found',
-  no_waiter_found: 'Sorry, no server registered to date',
+  no_waiter_found: 'Sorry, no waiter registered to date',
 
   // Open card reviews
   waiters: 'Waiters',

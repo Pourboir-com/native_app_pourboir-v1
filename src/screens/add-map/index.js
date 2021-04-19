928fb0547b2e1618f5e4dfab680ae0e9360e5f3f
Abdullah-Official <rugusakii@gmail.com>
2021-04-18 17:56:49 -0700

    Commit

--- src/screens/add-map/index.js
+++ src/screens/add-map/index.js
@@ -1,120 +1,165 @@
-import React, {useState} from 'react'
-import { KeyboardAvoidingView, StatusBar } from 'react-native'
-import { ImageBackground } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native'
-import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler'
-import GlobalHeader from '../../components/GlobalHeader'
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StatusBar } from 'react-native';
+import { ImageBackground } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+import GlobalHeader from '../../components/GlobalHeader';
 import i18n from '../../li8n';
 
-const AddMap = ({navigation}) => {
-    const [text, onChangeText] = React.useState();
-    const [text2, onChangeText2] = React.useState();
-    const [text3, onChangeText3] = React.useState();
-    const [text4, onChangeText4] = React.useState();
-    return (
-      <View style={styles.container}>
-        <StatusBar translucent={true} style="dark" />
-        <View style={{ flex: 1 }}>
-          <ImageBackground
-            style={{
-              width: '100%',
-              height: 100,
-              zIndex:111111
-            }}
-            source={require('../../assets/images/Group3.png')}
-          >
-            <GlobalHeader
-              arrow={true}
-              headingText={i18n.t('add_a_map')}
-              fontSize={17}
-              color={'black'}
-              navigation={navigation}
-              setting={false}
-              backgroundColor={'transparent'}
-              borderRadius={true}
-            />
-          </ImageBackground>
-        </View>
-        
-        <View style={{ flex: 7 }}>
-       <ScrollView>
-       <View style={{ marginHorizontal: 30, alignItems: 'center', marginTop:50 }}>
-                <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
-                  <Text style={{ color: '#1E272E', opacity: 1 , fontWeight:'bold', paddingBottom:3}}>{i18n.t('card_number')}</Text>
-                  <TextInput
-                    style={styles.inputsTopTow}
-                    onChangeText={onChangeText}
-                    value={text}
-                    placeholder="8888-8888-8888-8888"
-                    keyboardType="numeric"
-                  />
-                </View>
-                <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
-                  <Text style={{ color: '#1E272E', opacity: 1 , fontWeight:'bold', paddingBottom:3}}>{i18n.t('date_of_exp')}</Text>
-                  <TextInput
-                    style={styles.inputsTopTow}
-                    onChangeText={onChangeText2}
-                    value={text2}
-                    placeholder="MM/AA"
-                    keyboardType="text"
-                  />
-                </View>
-                <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
-                  <Text style={{ color: '#1E272E', opacity: 1 , fontWeight:'bold', paddingBottom:3}}>CVV</Text>
-                  <TextInput
-                    style={styles.inputsTopTow}
-                    onChangeText={onChangeText3}
-                    value={text3}
-                    placeholder="123"
-                    keyboardType="numeric"
-                  />
-                </View>
-                <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
-                  <Text style={{ color: '#1E272E', opacity: 1 , fontWeight:'bold', paddingBottom:3}}>{i18n.t('pays')}</Text>
-                  <TextInput
-                    style={styles.inputsTopTow}
-                    onChangeText={onChangeText4}
-                    value={text4}
-                    placeholder="France"
-                    keyboardType="text"
-                  />
-                </View>
-                </View>
-       </ScrollView>
-        </View>
-        <View style={{flex:1, alignItems:'center'}}>
-                    <TouchableOpacity activeOpacity={0.7} style={styles.btn_yellow}>
-                        <Text style={{fontSize:13}}>{i18n.t('save')}</Text>
-                    </TouchableOpacity>
-                </View>
+const AddMap = ({ navigation }) => {
+  const [text, onChangeText] = React.useState();
+  const [text2, onChangeText2] = React.useState();
+  const [text3, onChangeText3] = React.useState();
+  const [text4, onChangeText4] = React.useState();
+  return (
+    <View style={styles.container}>
+      <StatusBar translucent={true} style="dark" />
+      <View style={{ flex: 1 }}>
+        <ImageBackground
+          style={{
+            width: '100%',
+            height: 100,
+            zIndex: 111111,
+          }}
+          source={require('../../assets/images/Group3.png')}
+        >
+          <GlobalHeader
+            arrow={true}
+            headingText={i18n.t('add_a_map')}
+            fontSize={17}
+            color={'black'}
+            navigation={navigation}
+            setting={false}
+            backgroundColor={'transparent'}
+            borderRadius={true}
+          />
+        </ImageBackground>
       </View>
-    );
-}
 
-export default AddMap
+      <View style={{ flex: 7 }}>
+        <ScrollView>
+          <View
+            style={{
+              marginHorizontal: 30,
+              alignItems: 'center',
+              marginTop: 50,
+            }}
+          >
+            <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
+              <Text
+                style={{
+                  color: '#1E272E',
+                  opacity: 1,
+                  fontWeight: 'bold',
+                  paddingBottom: 3,
+                }}
+              >
+                {i18n.t('card_number')}
+              </Text>
+              <TextInput
+                style={styles.inputsTopTow}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder="8888-8888-8888-8888"
+                keyboardType="numeric"
+              />
+            </View>
+            <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
+              <Text
+                style={{
+                  color: '#1E272E',
+                  opacity: 1,
+                  fontWeight: 'bold',
+                  paddingBottom: 3,
+                }}
+              >
+                {i18n.t('date_of_exp')}
+              </Text>
+              <TextInput
+                style={styles.inputsTopTow}
+                onChangeText={onChangeText2}
+                value={text2}
+                placeholder="MM/AA"
+                keyboardType="text"
+              />
+            </View>
+            <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
+              <Text
+                style={{
+                  color: '#1E272E',
+                  opacity: 1,
+                  fontWeight: 'bold',
+                  paddingBottom: 3,
+                }}
+              >
+                CVV
+              </Text>
+              <TextInput
+                style={styles.inputsTopTow}
+                onChangeText={onChangeText3}
+                value={text3}
+                placeholder="123"
+                keyboardType="numeric"
+              />
+            </View>
+            <View style={{ alignItems: 'flex-start', marginBottom: 15 }}>
+              <Text
+                style={{
+                  color: '#1E272E',
+                  opacity: 1,
+                  fontWeight: 'bold',
+                  paddingBottom: 3,
+                }}
+              >
+                {i18n.t('pays')}
+              </Text>
+              <TextInput
+                style={styles.inputsTopTow}
+                onChangeText={onChangeText4}
+                value={text4}
+                placeholder="France"
+                keyboardType="text"
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <TouchableOpacity activeOpacity={0.7} style={styles.btn_yellow}>
+          <Text style={{ fontSize: 13 }}>{i18n.t('save')}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default AddMap;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F9F9F9',
-      },
-      inputsTopTow: {
-        borderColor: '#ccc',
-        borderWidth: 1,
-        width: 270,
-        padding: 10,
-        borderRadius: 10,
-        backgroundColor: '#fff',
-      },
-      btn_yellow:{
-        backgroundColor:'#FCDF6F',
-        width:300,
-        borderRadius:10,
-        padding:10,
-        paddingVertical:14,
-        alignItems:'center',
-        justifyContent:'center',
-        marginBottom:20
-      }
-})
-
+  container: {
+    flex: 1,
+    backgroundColor: '#F9F9F9',
+  },
+  inputsTopTow: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    width: 270,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+  },
+  btn_yellow: {
+    backgroundColor: '#FCDF6F',
+    width: 300,
+    borderRadius: 10,
+    padding: 10,
+    paddingVertical: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+});
--- src/screens/candidate-profile/index.js
+++ src/screens/candidate-profile/index.js
@@ -12,20 +12,23 @@ import styles from './styles';
 import { Button } from 'native-base';
 import { set } from 'react-native-reanimated';
 import I18n, { l } from 'i18n-js';
+import { KeyboardAvoidingView } from 'react-native';
+import { Dimensions } from 'react-native';
 
 const ProfileCandidate = ({ navigation }) => {
-  const [temp, setTemp] = useState();
+  const [temp, setTemp] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [education, setEducation] = useState('');
   const [experience, setExperience] = useState('');
   const [lastExperience, setLastExperience] = useState('');
+  const windowHeight = Dimensions.get('window').height;
 
   console.log(temp);
   return (
     <View style={styles.container}>
       <StatusBar translucent={true} style="dark" />
-      <View style={{ flex: 0 }}>
+      <View style={{ flex: 1, zIndex: 1111 }}>
         <ImageBackground
           style={{
             width: '100%',
@@ -45,105 +48,114 @@ const ProfileCandidate = ({ navigation }) => {
           />
         </ImageBackground>
       </View>
-      <ScrollView
-        keyboardShouldPersistTaps={'handled'}
-        bounces={false}
-        scrollEnabled={true}
-        style={{
-          width: '100%',
-        }}
-      >
-        <View style={styles.main_container}>
-          <View>
-            <Text style={styles.heading1}> {i18n.t('personal_info')}</Text>
-          </View>
-          <View>
-            <View style={styles.input_box}>
-              <Text style={styles.inputLabel}>{I18n.t('first_name')}</Text>
-              <TextInput
-                style={styles.inputsTopTow}
-                onChangeText={e => setFirstName(e)}
-                value={firstName}
-                placeholder="Christine"
-                placeholderTextColor={'#485460'}
-              />
+      <View style={{ flex: 5, marginTop: 10 }}>
+        <ScrollView
+          keyboardShouldPersistTaps={'handled'}
+          keyboardVerticalOffset={100}
+          bounces={false}
+          scrollEnabled={true}
+          style={{
+            width: '100%',
+          }}
+        >
+          <View style={styles.main_container}>
+            <View>
+              <Text style={styles.heading1}> {i18n.t('personal_info')}</Text>
             </View>
-            <View style={styles.input_box}>
-              <Text style={styles.inputLabel}>{i18n.t('last_name')}</Text>
-              <TextInput
-                style={styles.inputsTopTow}
-                onChangeText={e => setLastName(e)}
-                value={lastName}
-                placeholder="Zhou"
-                placeholderTextColor={'#485460'}
-              />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
+            <View>
               <View style={styles.input_box}>
-                <Text style={styles.inputLabel}>{i18n.t('experience')}</Text>
-                <View style={{ flexDirection: 'row' }}>
-                  <TextInput
-                    style={styles.smallInput}
-                    onChangeText={e => setExperience(e)}
-                    value={experience}
-                    placeholder=""
-                    maxLength={2}
-                    keyboardType="numeric"
-                    placeholderTextColor={'#485460'}
-                  />
-                  <Text style={styles.experience}> ans </Text>
+                <Text style={styles.inputLabel}>{I18n.t('first_name')}</Text>
+                <TextInput
+                  style={styles.inputsTopTow}
+                  onChangeText={e => setFirstName(e)}
+                  value={firstName}
+                  placeholder="Christine"
+                  placeholderTextColor={'#485460'}
+                />
+              </View>
+              <View style={styles.input_box}>
+                <Text style={styles.inputLabel}>{i18n.t('last_name')}</Text>
+                <TextInput
+                  style={styles.inputsTopTow}
+                  onChangeText={e => setLastName(e)}
+                  value={lastName}
+                  placeholder="Zhou"
+                  placeholderTextColor={'#485460'}
+                />
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <View style={styles.input_box}>
+                  <Text style={styles.inputLabel}>{i18n.t('experience')}</Text>
+                  <View style={{ flexDirection: 'row' }}>
+                    <TextInput
+                      style={styles.smallInput}
+                      onChangeText={e => setExperience(e)}
+                      value={experience}
+                      placeholder=""
+                      maxLength={2}
+                      keyboardType="numeric"
+                      placeholderTextColor={'#485460'}
+                    />
+                    <Text style={styles.experience}> ans </Text>
+                  </View>
+                </View>
+              </View>
+              <View style={styles.input_box}>
+                <Text style={styles.inputLabel}>
+                  {i18n.t('last_experience')}
+                </Text>
+                <TextInput
+                  style={styles.inputsTopTow}
+                  onChangeText={e => setLastExperience(e)}
+                  value={lastExperience}
+                  placeholder={i18n.t('passedat')}
+                  placeholderTextColor={'#485460'}
+                />
+              </View>
+              <View style={styles.input_box}>
+                <Text style={styles.inputLabel}>{i18n.t('education')}</Text>
+                <TextInput
+                  style={styles.inputsTopTow}
+                  onChangeText={e => setEducation(e)}
+                  value={education}
+                  placeholder="BTS Tourisme"
+                  placeholderTextColor={'#485460'}
+                />
+              </View>
+              <View style={styles.input_box}>
+                <Text style={styles.inputLabel}>Temps</Text>
+                <View
+                  style={
+                    temp !== ''
+                      ? styles.chooseButtons_container_active
+                      : styles.chooseButtons_container
+                  }
+                >
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    style={temp === 'Full' ? styles.btn1_active : styles.btn1}
+                    onPress={() => setTemp('Full')}
+                    value={temp}
+                  >
+                    <Text style={styles.timeTxt}>{i18n.t('full')}</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    style={
+                      temp === 'Partial' ? styles.btn2_active : styles.btn2
+                    }
+                    onPress={() => setTemp('Partial')}
+                    value={temp}
+                  >
+                    <Text style={styles.timeTxt}>{i18n.t('partial')}</Text>
+                  </TouchableOpacity>
                 </View>
               </View>
             </View>
-            <View style={styles.input_box}>
-              <Text style={styles.inputLabel}>{i18n.t('last_experience')}</Text>
-              <TextInput
-                style={styles.inputsTopTow}
-                onChangeText={e => setLastExperience(e)}
-                value={lastExperience}
-                placeholder={i18n.t('passedat')}
-                placeholderTextColor={'#485460'}
-              />
-            </View>
-            <View style={styles.input_box}>
-              <Text style={styles.inputLabel}>{i18n.t('education')}</Text>
-              <TextInput
-                style={styles.inputsTopTow}
-                onChangeText={e => setEducation(e)}
-                value={education}
-                placeholder="BTS Tourisme"
-                placeholderTextColor={'#485460'}
-              />
-            </View>
-            <View style={styles.input_box}>
-              <Text style={styles.inputLabel}>Temps</Text>
-              <View
-                style={
-                  temp !== ''
-                    ? styles.chooseButtons_container_active
-                    : styles.chooseButtons_container
-                }
-              >
-                <TouchableOpacity
-                  activeOpacity={0.7}
-                  style={temp === 'Full' ? styles.btn1_active : styles.btn1}
-                  onPress={() => setTemp('Full')}
-                  value={temp}
-                >
-                  <Text style={styles.timeTxt}>{i18n.t('full')}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  activeOpacity={0.7}
-                  style={temp === 'Partial' ? styles.btn2_active : styles.btn2}
-                  onPress={() => setTemp('Partial')}
-                  value={temp}
-                >
-                  <Text style={styles.timeTxt}>{i18n.t('partial')}</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
           </View>
-        </View>
+        </ScrollView>
+      </View>
+      <View style={{ flex: -2 }}>
         <View
           style={{
             alignItems: 'center',
@@ -151,12 +163,14 @@ const ProfileCandidate = ({ navigation }) => {
             marginTop: 20,
           }}
         >
-          {firstName ||
-          lastName ||
-          lastExperience ||
-          experience ||
-          education ||
-          temp === ' ' ? (
+          {(firstName.length &&
+            lastName.length &&
+            lastExperience.length &&
+            experience.length &&
+            education.length &&
+            temp.length > 0) ||
+          undefined ||
+          null ? (
             <Button disabled={false} style={styles.btn_yellow}>
               <Text style={{ fontSize: 14, fontFamily: 'ProximaNova' }}>
                 {i18n.t('candidate_btn')}
@@ -170,7 +184,7 @@ const ProfileCandidate = ({ navigation }) => {
             </Button>
           )}
         </View>
-      </ScrollView>
+      </View>
     </View>
   );
 };

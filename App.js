8002f2480ef5f87972d53f019170987bbbb9c636
Muhammad Salman Siddiqui <salmansidd991@gmail.com>
2021-04-21 03:00:32 +0500

    update

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useReducer, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation/AppNavigator';
 import Context from './src/contextApi/context';
 import Reducer from './src/contextApi/reducer';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  const PUSH_ENDPOINT = 'https://easy-soup.glitch.me/token';
+
+  const registerForPushNotifications = async () => {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (status !== 'granted') {
+      alert('No notification permissions!');
+      return;
+    }
+
+    // Get the token that identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+    console.log(token);
+
+    // POST the token to your backend server from where you can retrieve it to send push notifications.
+    return fetch(PUSH_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: {
+          value: token,
+        },
+      }),
+    });
+  };
+
+  useEffect(() => {
+    registerForPushNotifications();
+  }, []);
+
   const initialState = useContext(Context);
   const [state, dispatch] = useReducer(Reducer, initialState);
   return (
